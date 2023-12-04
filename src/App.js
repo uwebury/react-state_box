0089c9fb87dd7f99a67f2725efbe14b9c8415d83
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-04 15:59:04 +0100

    Refactored code for re-rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  let [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive((isActive = !isActive));
     // Check that the value changes correctly.
     console.log(isActive);
   }
